d6b758bbc6cd8420218ebdaba80fefcff9a22850
Indra Puji Novirwan <indrapuji@Indras-MacBook.local>
2020-06-21 00:50:49 +0700

    navbar

--- grabClone/App.js
+++ grabClone/App.js
@@ -1,114 +1,95 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, View, Text, Image} from 'react-native';
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <View style={{backgroundColor: 'blue', flex: 1, marginTop: 25}}>
+      <View style={{backgroundColor: 'red', flex: 1}} />
+      <View
+        style={{height: 59, backgroundColor: 'white', flexDirection: 'row'}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View style={{width: 24, height: 24, backgroundColor: '#ffffff'}}>
+            <Image
+              source={require('./assets/nav/home.png')}
+              style={{width: 24, height: 24}}
+            />
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          <Text style={{fontSize: 11, color: '#676767', marginTop: 8}}>
+            Home
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View style={{width: 24, height: 24, backgroundColor: '#ffffff'}}>
+            <Image
+              source={require('./assets/nav/activity.png')}
+              style={{width: 24, height: 24}}
+            />
+          </View>
+          <Text style={{fontSize: 11, color: '#676767', marginTop: 8}}>
+            Activity
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View style={{width: 24, height: 24, backgroundColor: '#ffffff'}}>
+            <Image
+              source={require('./assets/nav/payment.png')}
+              style={{width: 24, height: 24}}
+            />
+          </View>
+          <Text style={{fontSize: 11, color: '#676767', marginTop: 8}}>
+            Payment
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View style={{width: 24, height: 24, backgroundColor: '#ffffff'}}>
+            <Image
+              source={require('./assets/nav/inbox.png')}
+              style={{width: 24, height: 24}}
+            />
+          </View>
+          <Text style={{fontSize: 11, color: '#676767', marginTop: 8}}>
+            Inbox
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View style={{width: 24, height: 24, backgroundColor: '#ffffff'}}>
+            <Image
+              source={require('./assets/nav/account.png')}
+              style={{width: 24, height: 24}}
+            />
+          </View>
+          <Text style={{fontSize: 11, color: '#676767', marginTop: 8}}>
+            Account
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
