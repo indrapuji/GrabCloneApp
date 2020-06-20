89f7affcfa3be12b7197aa9ec80fde7ba5aea3b2
Indra Puji Novirwan <indrapuji@gmail.com>
2020-06-21 01:58:31 +0700

    searchbar

--- grabClone/App.js
+++ grabClone/App.js
@@ -1,10 +1,66 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 const App: () => React$Node = () => {
   return (
-    <View style={{backgroundColor: 'blue', flex: 1, marginTop: 25}}>
-      <View style={{backgroundColor: 'red', flex: 1}} />
+    <View style={{flex: 1}}>
+      <ScrollView>
+        <View style={{flex: 1}}>
+          <View
+            style={{
+              paddingBottom: 13,
+              paddingHorizontal: 13,
+              paddingTop: 38,
+              backgroundColor: '#34C072',
+            }}>
+            <View style={{position: 'relative'}}>
+              <TextInput
+                placeholder="Offers, food, and place to go"
+                style={{
+                  borderWidth: 1,
+                  borderRadius: 5,
+                  height: 40,
+                  fontSize: 15,
+                  borderColor: '#3FAB70',
+                  paddingLeft: 98,
+                  paddingRight: 20,
+                  backgroundColor: 'white',
+                }}
+              />
+              <Image
+                source={require('./assets/home/magnifying.png')}
+                style={{
+                  position: 'absolute',
+                  height: 15,
+                  width: 15,
+                  top: 13,
+                  left: 75,
+                }}
+              />
+              <Image
+                source={require('./assets/home/scan.png')}
+                style={{
+                  position: 'absolute',
+                  width: 38,
+                  height: 38,
+                  backgroundColor: '#F7F7F7',
+                  top: 1,
+                  left: 1,
+                  borderTopLeftRadius: 5,
+                  borderBottomLeftRadius: 5,
+                }}
+              />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
       <View
         style={{height: 59, backgroundColor: 'white', flexDirection: 'row'}}>
         <View
